b0469d0f3933312c042856ed583c8f01e2383aa5
vivek <vivekgandharkar@outlook.com>
2021-05-09 19:21:49 +0530

    Initialized polls app.

--- temp_polls/settings.py
+++ temp_polls/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'polls',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
