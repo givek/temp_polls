fadcd1cd289b1aacab2f3b7cd1f3611bafeef23d
vivek <vivekgandharkar@outlook.com>
2021-05-11 20:37:29 +0530

    Created urls from views.

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,8 @@ from polls import views
 
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('polls/', views.create_polls, name='create_polls'),
+    path('polls/<uuid:poll_id>/vote/', views.vote, name='vote'),
+    path('polls/<uuid:slug>/results/', views.ResultsView.as_view(), name='results'),  
 ]
